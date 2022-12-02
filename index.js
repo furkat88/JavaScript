20714a15aa4073850614e377635789c083086d36
furkat88 <sattorov.furkat88@gmail.com>
2022-12-02 20:23:37 +0500

    nbnasdasd

--- index.js
+++ index.js
@@ -1,136 +1,60 @@
-const modal_one = document.querySelector('.modal_one')
-const modal_two = document.querySelector('.modal_two')
-const plus_one = document.querySelector('.plus_one')
-const plus_two = document.querySelector('.plus_two')
-const cover = document.querySelectorAll('.cover')
-const next = document.querySelector('.next')
-const prev = document.querySelector('.prev')
+const input = document.querySelector("#todo_text");
+const addButton = document.querySelector(".add_todo_button");
+const todosContainer = document.querySelector(".todos_container");
 
-modal_one.onmouseenter = () => {
-  plus_one.style.background = 'black'
-  plus_one.style.color = 'white'
+function getdate() {
+  const date = new Date();
+  const hours = date.getHours();
+  const minutes = date.getMinutes();
+  const day = date.getDate();
+  const month = date.getMonth();
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  return `${hours}:${minutes} "${day} ${monthNames[month]}"`;
 }
 
-modal_one.onmouseleave = () => {
-  plus_one.style.background = 'white'
-  plus_one.style.color = 'black'
+function creatTodo(text) {
+  const box = document.createElement("div");
+  const h3 = document.createElement("h3");
+  const p = document.createElement("p");
+  const close = document.createElement("div");
+  box.classList.add("todo_box");
+  close.classList.add("todo_close");
+  close.innerHTML = "&#10006";
+  h3.innerText = text;
+  p.innerText = getdate();
+  box.appendChild(h3);
+  box.appendChild(p);
+  box.appendChild(close);
+
+  return box;
 }
 
-modal_two.onmouseenter = () => {
-  plus_two.style.background = 'black'
-  plus_two.style.color = 'white'
+const todos = [];
+
+function render(arr) {
+  arr.forEach((todo) => todosContainer.appendChild(todo));
 }
 
-modal_two.onmouseleave = () => {
-  plus_two.style.background = 'white'
-  plus_two.style.color = 'black'
-}
+addButton.onclick = () => {
+  let inputValue = input.value;
+  let todo = creatTodo(inputValue);
+  todos.push(todo);
+  render(todos);
+};
 
-let percent = 0
-
-  if (percent === 0) prev.classList.add('displayNone')
-  next.onclick = () => { 
-    if(percent === -200) return 
-    percent -= 100
-    cover.forEach(el => {
-      el.style.left = `${percent}%`
-    })
-    if (percent !== 0) prev.classList.remove('displayNone')
-    if (percent == -200) next.classList.add('displayNone')
-  }
-  
-  prev.onclick = () => { 
-    if(percent === 0) return 
-    percent += 100
-    cover.forEach(el => {
-      el.style.left = percent + '%'
-    })
-    if (percent === 0) prev.classList.add('displayNone')
-    if (percent !== -200) next.classList.remove('displayNone')
-  }
-
-
-const blue = document.querySelector('#blue')
-const midnight = document.querySelector('#midnight')
-const pink = document.querySelector('#pink')
-const starlight = document.querySelector('#starlight')
-const red = document.querySelector('#red')
-const h4color = document.querySelector('.h4color')
-
-const arrColors = [blue, midnight, pink, starlight, red]
-
-function blabla(arr) {
-
-  let col = h4color.innerHTML
-
-  arr.forEach(el => {
-      el.onclick = () => {
-
-        arr.forEach((x) => {
-          x === el ? x.style.border = `2px solid ${
-            x === blue ? '#0b4268' : x === midnight ? '#42474d' : 
-            x === pink ? '#fbe2dd' : x === starlight ? '#fbf7f4' : 'red'
-          }` : x.style.border = ''
-        })
-
-
-        cover.forEach((y,i)=> {
-          if (el === blue && i !== 0) {
-            y.classList.add('displayNone')
-            cover[0].classList.remove('displayNone')
-            col = 'Color: Blue'
-          }
-          if (el === midnight && i !== 1) {
-            y.classList.add('displayNone')
-            cover[1].classList.remove('displayNone')
-            col = 'Color: Midnight'
-          }
-          if (el === pink && i !== 2) {
-            y.classList.add('displayNone')
-            cover[2].classList.remove('displayNone')
-            col = 'Color: Pink'
-          }
-          if (el === starlight && i !== 4) {
-            y.classList.add('displayNone')
-            cover[4].classList.remove('displayNone')
-            col = 'Color: Starlight'
-          }
-          if (el === red && i !== 3) {
-            y.classList.add('displayNone')
-            cover[3].classList.remove('displayNone')
-            console.log(cover[3]);
-            col = 'Color: Red'
-          }
-        })
-      }
-
-      
-      el.onmouseenter = () => {
-          if(el === blue) {
-            h4color.innerHTML = 'Color: Blue'
-          }
-          if(el === midnight) {
-            h4color.innerHTML = 'Color: Midnight'
-          }
-          if(el === pink) {
-            h4color.innerHTML = 'Color: Pink'
-          }
-          if(el === starlight) {
-            h4color.innerHTML = 'Color: Starlight'
-          }
-          if(el === red) {
-            h4color.innerHTML = 'Color: Red'
-          }
-      }
-
-      el.onmouseleave = () => {
-        h4color.innerHTML = col
-    }
-  })
-}
-
-blabla(arrColors)
-
-console.log(cover);
 
 
